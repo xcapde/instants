08491f5922e13b2190287dccae02c8819d73ecb4
xcapde <xcapde@hotmail.com>
2022-06-20 19:33:35 +0200

    new style greens light, style ready

--- src/components/update/Update.jsx
+++ src/components/update/Update.jsx
@@ -5,13 +5,13 @@ export function Update() {
             <div className="update_box">
                 <div className="inputs_box">
                     <label for="title">Title:</label>
-                    <input className="title_input" name="title" id="title" type="text" placeholder="Title"/>
+                    <input className="title_input" name="title" id="title" type="text" placeholder="Write a title"/>
 
                     <label for="description">Description:</label>
-                    <textarea className="description_input"  name="description" id="description" placeholder="Description.."/>
+                    <textarea className="description_input"  name="description" id="description" placeholder="Share a description"/>
                     
                     <label for="imageUrl">Image URL:</label>
-                    <input className="imgurl_input" name="imageUrl" id="imageUrl" type="text" placeholder="Image URL"/>
+                    <input className="imgurl_input" name="imageUrl" id="imageUrl" type="text" placeholder="URL"/>
                     
                     <div className="update_btns">                    
                         <button type="button" className="cancel_btn">CANCEL</button>
